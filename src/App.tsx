fddf2836845194727e3145bce9badc49105200a2
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-03-07 10:30:50 +0200

    Update route to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import './App.css';
 import Root from './routes/Root';
 import ErrorPage from './routes/ErrorPage';
 import Projects from './routes/Projects';
@@ -7,24 +7,25 @@ import About from './routes/About';
 import Resume from './routes/Resume';
 import Home from './routes/Home';
 import Blogs from './routes/Blogs';
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: '/', element: <Navigate to="/home" replace /> },
+        { path: '/home', element: <Home /> },
+        { path: '/about', element: <About /> },
+        { path: '/projects', element: <Projects /> },
+        { path: '/resume', element: <Resume /> },
+        { path: '/blogs', element: <Blogs /> },
+      ],
+    },
+  ]);
 
- const router = createBrowserRouter([
-   {
-     path: '/',
-     element: <Root />,
-     errorElement: <ErrorPage />,
-     children: [
-       { path: '/home', element: <Home /> },
-       { path: '/about', element: <About /> },
-       { path: '/projects', element: <Projects /> },
-       { path: '/resume', element: <Resume /> },
-       { path: '/blogs', element: <Blogs /> },
-     ],
-   },
- ]);
-
- return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
