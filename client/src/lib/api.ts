f6bfa9c1ca59a2fcb9a510a0037a6eebdb6224c9
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-04-12 16:36:42 +0300

    Update api url

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = 'http://localhost:1337/api/blogs';
+const API_URL = import.meta.env.VITE_API_BASE_URL + '/api/blogs';
 
 // Fetch all blogs
 export const fetchBlogs = async (locale: string) => {
