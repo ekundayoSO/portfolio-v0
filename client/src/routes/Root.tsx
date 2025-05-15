2ceddbb5418e0e5dfcb6157718928b6793fecb02
Sulaimon Ekundayo <sulaimon.ekundayo@tutors.es>
2025-05-15 19:46:34 +0300

    Update footer and root components

--- client/src/routes/Root.tsx
+++ client/src/routes/Root.tsx
@@ -1,18 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import { Footer } from "../components/Footer";
-import BackToTopButton from "../components/BackToTopButton";
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import { Footer } from '../components/Footer';
+import BackToTopButton from '../components/BackToTopButton';
 
-const Root:React.FC = () => {
+const Root: React.FC = () => {
   return (
     <div className="d-flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow">
+      <main className="flex-1">
         <Outlet />
-        <BackToTopButton />
       </main>
+      <BackToTopButton />
       <Footer />
     </div>
   );
-}
+};
 export default Root;
